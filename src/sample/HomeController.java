50851f63d0efa6f2108172959f58a7988250c1ec
Nikolai <nik-neman@mail.ru>
2022-07-10 21:20:26 +0200

    Lesson #10

---
+++ src/sample/HomeController.java
@@ -0,0 +1,25 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane ImageButtonHome;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
